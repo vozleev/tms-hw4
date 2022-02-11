9e95ed25a570b3fdd7213f117154952a58d640a3
Yury Vozleev <yury.vozleev@mobileup.ru>
2022-02-11 22:04:21 +0300

    Initial commit

---
+++ something/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  something
+//
+//  Created by Yury Vozleev on 11.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

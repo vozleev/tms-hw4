b5f8f7da2dbea2eda1b2cdb4bd9f950360e85b37
Yury Vozleev <yury.vozleev@mobileup.ru>
2022-02-11 22:11:13 +0300

    Complete hw 4

--- something/ViewController.swift
+++ something/ViewController.swift
@@ -9,9 +9,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var a = 2
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        
+        var array = [1, 2, 3]
+
+        let secondArray = [4, 5, 6]
     }
 
 
